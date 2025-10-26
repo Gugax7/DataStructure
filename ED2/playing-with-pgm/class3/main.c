e03040112f0b4295d95615d873c20cb146f000b7
Gustavo Salmazo <gux.salmazo@gmail.com>
2025-10-26 20:21:40 -0300

    export logic understood

--- ED2/playing-with-pgm/class3/main.c
+++ ED2/playing-with-pgm/class3/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define INDEX_FILE "index.txt"
+#define DATABASE_FILE "database.bin"
+
 typedef struct{
   int* values;
   int width, height;
@@ -10,7 +13,7 @@ typedef struct{
 }PgmImage;
 
 typedef struct{
-  char *fileName[256];
+  char fileName[256];
   long offset;
   long size;
 }Index;
@@ -33,11 +36,20 @@ void convertImageToStruct(FILE *pgm, PgmImage *object){
   strcpy(object->config, config);
 
   // jump comments
-  // size x size
+  char c;
 
+  while((c = getc(pgm)) == '#'){
+    while((c = fgetc(pgm)) != '\n');
+  }
+
+  ungetc(c, pgm);
+
+  // size x size
   fscanf(pgm, "%d %d", &width, &height);
+  // gray scale
   fscanf(pgm, "%d", &maxGray);
 
+  // writing values
   object->width = width;
   object->height = height;
   object->grayMax = maxGray;
@@ -53,14 +65,14 @@ void convertImageToStruct(FILE *pgm, PgmImage *object){
   }
 }
 
-int importImage(char * pgmFile, char * databaseFile, char* indexFile){
+int importImage(char * pgmFile){
 
   FILE* pgmStream;
   FILE* databaseStream;
   FILE* indexStream;
 
   pgmStream = fopen(pgmFile, "r");
-  databaseStream = fopen(databaseFile, "ab+");
+  databaseStream = fopen(DATABASE_FILE, "ab+");
 
   if(!pgmStream || !databaseStream) return 500;
 
@@ -77,28 +89,88 @@ int importImage(char * pgmFile, char * databaseFile, char* indexFile){
   fwrite(&imageObject.grayMax, sizeof(int), 1, databaseStream);
   fwrite(imageObject.values, sizeof(int), imageObject.height * imageObject.width, databaseStream);
 
-  indexStream = fopen(indexFile, "a");
+  indexStream = fopen(INDEX_FILE, "a");
 
   fseek(databaseStream, 0, SEEK_END);
 
-  const size = ftell(databaseStream) - currentOffSet;
+  long size = ftell(databaseStream) - currentOffSet;
 
-  fprintf(indexFile, "%s %ld %ld",
+  fprintf(indexStream, "%s %ld %ld\n",
     pgmFile,
     currentOffSet,
     size);
 
   fclose(pgmStream);
   fclose(databaseStream);
+  fclose(indexStream);
 
   free(imageObject.values);
   
   return 0;
 }
 
+int exportImage(char * imageName, char *outputName){
+  FILE* outputStream = fopen(outputName, "w");
+  FILE* indexStream = fopen(INDEX_FILE, "r");
+
+  char inFileName[256];
+  long offset;
+  long size;
+  int position = 0;
+
+
+  // Find the index with this outputName;
+  do{
+    position++;
+    if(fscanf(indexStream, "%s %ld %ld",inFileName, &offset, &size) != 3){
+      return 1;
+    };
+  }while(strcmp(inFileName, imageName) != 0);
+
+  //printf("on position %d: found!, %s %ld %ld", position, inFileName, offset, size);
+
+  // from here i actually have this images offsets, sizes and positions
+
+  FILE* databaseStream = fopen(DATABASE_FILE, "rb");
+
+  PgmImage outputObject;
+
+  fseek(databaseStream, offset, SEEK_SET);
+
+  fread(outputObject.config, sizeof(char), 3, databaseStream);
+  fread(&outputObject.width, sizeof(int), 1, databaseStream);
+  fread(&outputObject.height, sizeof(int), 1, databaseStream);
+  fread(&outputObject.grayMax, sizeof(int), 1, databaseStream);
+
+  outputObject.values = (int*)malloc(sizeof(int) * outputObject.width * outputObject.height);
+  if(outputObject.values == NULL){
+    printf("it failed!\n");
+    return 1;
+  }
+
+  fread(outputObject.values, sizeof(int), outputObject.width * outputObject.height, databaseStream);
+
+  fprintf(outputStream, "%s\n", outputObject.config);
+  fprintf(outputStream, "%d %d\n", outputObject.width, outputObject.height);
+  fprintf(outputStream, "%d\n", outputObject.grayMax);
+
+  for(int i = 0; i < outputObject.width * outputObject.height; i++){
+    fprintf(outputStream, "%d ", outputObject.values[i]);
+  }
+  fclose(outputStream);
+  fclose(indexStream);
+  fclose(databaseStream);
+
+  return 0;
+}
+
 int main(){
   
-  importImage("barbara.pgm", "output.bin", "index.txt");
+  // importImage("barbara.pgm");
+  // importImage("seila.pgm");
+  // importImage("limiar.pgm");
+
+  exportImage("seila.pgm","dk.pgm");
 
   return 0;
 }
